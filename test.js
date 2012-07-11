f932e76882136d14a8044c74ba529bd737deb2fd
David Pierce <david.dean.pierce@gmail.com>
2012-07-11 15:47:37 -0700

    Add test for event filters

--- test.js
+++ test.js
@@ -24,7 +24,7 @@ test('returns the key length for an object', function () {
 
 module('buildQueryUrl tests');
 test('returns baseUrl for an empty query', function () {
-  var result = testHarness.buildQueryUrl('http://baseurl', {}); 
+  var result = testHarness.buildQueryUrl('http://baseurl', {});
   equal(result, 'http://baseurl');
 });
 
@@ -51,4 +51,90 @@ test('handles the case where the base URL has a trailing slash', function () {
   result = testHarness.buildQueryUrl('http://baseurl/', query);
 
   equal(result, 'http://baseurl?country=USA&since=2010-01-01');
+});
+
+module('filterUnusableEvents tests');
+test('rejects events with the wrong status', function () {
+  var result, events = [
+    {
+      event_status: 'C',
+      location: {
+        lat: 10,  
+        lng: 10
+      }
+    },
+    {
+      event_status: 'P',
+      location: {
+        lat: 10,  
+        lng: 10
+      }
+    },
+    {
+      event_status: 'T',
+      location: {
+        lat: 10,  
+        lng: 10
+      }
+    }
+  ];
+
+  result = testHarness.filterUnusableEvents(events);
+
+  equal(result.length, 0);
+});
+
+test('only takes events with good or working status', function () {
+  var result, events = [
+    {
+      event_status: 'W',
+      location: {
+        lat: 10,  
+        lng: 10
+      }
+    },
+    {
+      event_status: 'G',
+      location: {
+        lat: 10,  
+        lng: 10
+      }
+    },
+    {
+      event_status: 'T',
+      location: {
+        lat: 10,  
+        lng: 10
+      }
+    }
+  ];
+
+  result = testHarness.filterUnusableEvents(events);
+
+  equal(result.length, 2);
+});
+
+test('only takes events with latitude and longitude', function () {
+  var result, events = [
+    {
+      event_status: 'W',
+      location: {
+        lat: 10,  
+        lng: 10
+      }
+    },
+    {
+      event_status: 'G'
+    },
+    {
+      event_status: 'G',
+      location: {
+        lat: 10,  
+      }
+    }
+  ];  
+
+  result = testHarness.filterUnusableEvents(events);
+  
+  equal(result.length, 1);
 });
