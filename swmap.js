5916801e818c226e6046f81c679019e2685db42b
David Pierce <david.dean.pierce@gmail.com>
2012-07-11 15:50:01 -0700

    Stub out more functions. Add map related stuff

--- swmap.js
+++ swmap.js
@@ -1,7 +1,8 @@
 "use strict";
 
-(function ($) {
-  var buildQueryUrl, testHarness, objectSize;
+(function ($, mapsApi) {
+  var buildQueryUrl, testHarness, objectSize, 
+      processEventData, initializeMap, filterUnusableEvents;
 
   if (window.testHarness) {
     testHarness = window.testHarness;
@@ -52,6 +53,51 @@
   if (testHarness) { testHarness.buildQueryUrl = buildQueryUrl; }
 
   /**
+   * Initialize the map via the Google Maps API and return
+   * the instance
+   */
+  initializeMap = function (mapId, mapOptions) {
+    return new mapsApi.Map($(mapId), mapOptions); 
+  }
+
+  /**
+   * Given a list of events, only return the ones we can
+   * use on the map. In this case, usable events are in
+   * a Good or Working state, and have location data attached
+   * on them
+   */
+  filterUnusableEvents = function (events) {
+    var goodEvents = [];
+    $.each(events, function (idx, event) {
+      if (event.event_status && (event.event_status === 'G' || event.event_status === 'W')) {
+        if (event.location && event.location['lat'] && event.location['lng']) {
+          goodEvents.push(event);
+        } 
+      } 
+    });
+
+    return goodEvents;
+  };
+  if (testHarness) { testHarness.filterUnusableEvents = filterUnusableEvents; }
+
+  /**
+   * A function to receive data from the API server, process
+   * it, and kick off other processing activity
+   *
+   * @data - An array of JSON objects representing events
+   */
+  processEventData = function (data, settings) {
+    var map, eventWorkingSet;
+
+    map = initializeMap(settings.mapId, settings.mapSettings);
+
+    // Loop through the events returned from the server and filter
+    // out those that don't meet the criteria for our map
+    eventWorkingSet = filterUnusableEvents(data);
+  };
+  if (testHarness) { testHarness.processEventData = processEventData; }
+
+  /**
    * This plugin creates a map based on data points
    * provided by the SWOOP API
    *
@@ -62,7 +108,17 @@
     var defaults, settings, apiUrl;
 
     defaults = {
-      url: 'http://swoop.startupweekend.org/events'
+      url: 'http://swoop.startupweekend.org/events',
+      query: {},
+      mapId: mapId,
+      mapSettings: {
+        center: new mapsApi.LatLng(30, 20),
+        zoom: 2,
+        minZoom: 2,
+        maxZoom: 8,
+        zoomControl: true,
+        mapsTypeId: mapsApi.MapTypeId.ROADMAP
+      }
     };
 
     // Grab the client's options and set up options
@@ -72,5 +128,11 @@
     // Build the API URL based on query parameters
     // if they are supplied
     apiUrl = buildQueryUrl(settings.url, settings.query);
+
+    $.ajax({
+      dataType: 'jsonp',
+      url: apiUrl,
+      success: function (data) { processEventData (data, settings); }
+    });
   };
-}(jQuery));
+}(jQuery, google.maps));
